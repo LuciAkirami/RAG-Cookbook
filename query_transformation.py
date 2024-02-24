a4d3c600f0e35dddfdf810ba46bb403d7a68099e
LuciAkirami <ajsubscriber001@gmail.com>
2024-02-24 21:44:22 +0530

    Added Step Back Method

--- query_transformation.py
+++ query_transformation.py
@@ -206,3 +206,179 @@ rewrite_retrieve_read_chain = (
 response = rewrite_retrieve_read_chain.invoke(distracted_query)
 
 print("Rewrite-Retrieve-Read Response for Distracted Query: ",response)
+
+
+# ---------------------------------------------- Method 3 ----------------------------------------------
+"""## Step Back Prompting """
+"""
+The prompting technique called "Step-Back" prompting can improve performance on complex questions by 
+first asking a "step back" question. This can be combined with regular question-answering applications 
+by then doing retrieval on both the original and step-back question. The Step Back Prompting helps in both 
+RAG and Non-RAG context
+
+Eg:
+You are an expert at world knowledge. Your task is to step back and paraphrase a question to a more generic step-back question, which is easier to answer.
+
+Here are a few examples:
+
+Original Question: Which position did Knox Cunningham hold from May 1955 to Apr 1956? 
+Stepback Question: Which positions have Knox Cunning- ham held in his career?
+
+Original Question: Who was the spouse of Anna Karina from 1968 to 1974? S
+tepback Question: Who were the spouses of Anna Karina?
+
+Original Question: Which team did Thierry Audel play for from 2007 to 2008? 
+Stepback Question: Which teams did Thierry Audel play for in his career?
+"""
+
+
+# Creating a function for the search tool
+from langchain.tools import DuckDuckGoSearchRun
+
+search = DuckDuckGoSearchRun()
+
+def retriever(query):
+    return search.run(query)
+
+
+# Creating a prompt template for assigning few shot examples
+from langchain_core.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate
+
+# Below are the few shot examples
+examples = [
+    {
+        "input": "Could the members of The Police perform lawful arrests?",
+        "output": "what can the members of The Police do?",
+    },
+    {
+        "input": "Jan Sindel’s was born in what country?",
+        "output": "what is Jan Sindel’s personal history?",
+    },
+]
+
+# Defining our ChatPrompt Template
+example_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("human", "{input}"),
+        ("ai", "{output}"),
+    ]
+)
+
+# Finally we pass the Prompt Template with the examples to the FewShotChatMessagePromptTempate
+few_shot_prompt = FewShotChatMessagePromptTemplate(
+    example_prompt=example_prompt,
+    examples=examples,
+)
+
+# print(few_shot_prompt)
+
+
+# Creating the final Prompt
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You are an expert at world knowledge. Your task is to step back and paraphrase a question to a more generic step-back question, which is easier to answer. Here are a few examples:""",
+        ),
+        # Few shot examples
+        few_shot_prompt,
+        # New question
+        ("user", "{question}"),
+    ]
+)
+
+# print(prompt.messages)
+
+
+# Creating the Model object
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# SystemMessages are not yet supported in ChatGoogleGenerativeAI, hence set convert_system_message_to_human to True, 
+# to automatically convert the leading SystemMessage to a HumanMessage,
+llm = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
+
+
+# Lets generate some questions
+# defining our step_back question generation chain with LCEL
+step_back_qn_gen = prompt | llm | StrOutputParser()
+
+question = "was chatgpt around while trump was president?"
+
+step_back_question = step_back_qn_gen.invoke({"question": question})
+
+print("Actual Question:",question)
+print("Step Back Question:",step_back_question)
+
+
+# Checking the Retrieval/Context without Step Back
+retrieved_data = retriever(question)
+print("Retrieved Data - Without Step Back:\n",retrieved_data)
+
+# Checking the Retrieval/Context with Step Back
+retrieved_data = retriever(step_back_qn_gen.invoke({"question": question}))
+print("Retrieved Data - with Step Back:\n",retrieved_data)
+
+
+# Creating the Final Prompt that takes in both Original and Step Back Context
+response_prompt_template = """You are an expert of world knowledge. I am going to ask you a question. Your response should be comprehensive and not contradicted with the following context if they are relevant. Otherwise, ignore them if they are not relevant.
+
+{normal_context}
+{step_back_context}
+
+Original Question: {question}
+Answer:"""
+
+response_prompt = ChatPromptTemplate.from_template(response_prompt_template)
+
+# Uncomment and Run the below if you want the prompt from langchainhub instead of the above
+# from langchain import hub
+
+# response_prompt = hub.pull("langchain-ai/stepback-answer")
+
+
+# Creating our Final Chain
+from langchain_core.runnables import RunnableLambda
+
+chain = (
+    {
+        # Retrieve context using the normal question
+        "normal_context": RunnableLambda(lambda x: x["question"]) | retriever,
+        # Retrieve context using the step-back question
+        "step_back_context": step_back_qn_gen | retriever,
+        # Pass on the question
+        "question": lambda x: x["question"],
+    }
+    | response_prompt
+    | llm
+    | StrOutputParser()
+)
+
+
+# Testing the previous prompt with the Step Back Method
+step_back_approach_response = chain.invoke({"question": question})
+print("Answer with Step Back Approach:\n",step_back_approach_response)
+
+
+# Testing the BaseLine Approach without Step Back
+response_prompt_template = """You are an expert of world knowledge. I am going to ask you a question. Your response should be comprehensive and not contradicted with the following context if they are relevant. Otherwise, ignore them if they are not relevant.
+
+{normal_context}
+
+Original Question: {question}
+Answer:"""
+response_prompt = ChatPromptTemplate.from_template(response_prompt_template)
+
+chain = (
+    {
+        # Retrieve context using the normal question
+        "normal_context": RunnableLambda(lambda x: x["question"]) | retriever,
+        # Pass on the question
+        "question": lambda x: x["question"],
+    }
+    | response_prompt
+    | llm
+    | StrOutputParser()
+)
+
+without_stepback_response = chain.invoke({"question": question})
+print("Answer without Step Back:\n",without_stepback_response)
